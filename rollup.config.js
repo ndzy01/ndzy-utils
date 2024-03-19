88adfaa6c1ea4214bdd987aee08be39f0051205e
ndzy <2326962137@qq.com>
2024-03-20 05:58:26 +0800

    init

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.esm.js',
+      format: 'es',
+    },
+    {
+      file: 'dist/index.umd.js',
+      format: 'umd',
+      name: 'index.umd.js',
+    },
+  ],
+  plugins: [commonjs(), typescript()],
+};
