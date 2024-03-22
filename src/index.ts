1f317ed9933e9d97c64d909ca25661ca493b0246
ndzy <2326962137@qq.com>
2024-03-22 08:05:37 +0800

    chore: 1

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as service } from './http';
 export { setupStores } from './store/setupStores';
 export { makeAutoObservable } from 'mobx';
 export { observer } from 'mobx-react-lite';
+export { default as login } from './login';
