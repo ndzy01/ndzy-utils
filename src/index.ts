00edfb480bc90ab7c4274052fba13657ec4f9b47
ndzywork998 <zhang.yi@pitayagames.com>
2024-07-10 11:21:43 +0800

    优化

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import "./index.css"
 
 export { setupStores } from "./lib/setupStore"
-export { Button } from "./components/ui/button"
+export * from "./components/ui/button"
+export * from "./components/ui/accordion"
+export * from "./components/ui/card"
+export * from "./components/ui/input"
 export * from "./utils"
