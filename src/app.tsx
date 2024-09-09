828b66345524a4148e99e51d18be1e7973373b6b
ndzywork998 <zhang.yi@pitayagames.com>
2024-09-09 10:54:42 +0800

    优化

--- src/app.tsx
+++ src/app.tsx
@@ -19,6 +19,7 @@ interface NdzyContextType {
       find: (id: string) => Promise<void>
       save: (id: string, params: any) => Promise<void>
       create: (params: any) => Promise<void>
+      uploadImg: (params: any) => Promise<any>
     }
   }
 }
@@ -65,6 +66,10 @@ const App = (props: { children: React.ReactNode }) => {
     query().then()
   }
 
+  const uploadImg = async (params: any) => {
+    return service({ url: "/imgs", method: "POST", data: params })
+  }
+
   const find = async (id: string) => {
     setLoading(true)
     const data: any = await service({ url: `/article/${id}`, method: "GET" })
@@ -85,7 +90,7 @@ const App = (props: { children: React.ReactNode }) => {
       service={service}
       articles={articles}
       article={article}
-      api={{ article: { query, del, find, save, create } }}
+      api={{ article: { query, del, find, save, create, uploadImg } }}
     >
       {children}
     </NdzyProvider>
